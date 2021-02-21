c57b226f7f1add71d062eb252c91b8eff33c6d8d
Andrew Lee <andrewleee10@gmail.com>
2021-02-21 12:02:44 -0800

    updated code

--- app.js
+++ app.js
@@ -6,43 +6,27 @@ document.getElementById('currentDay').append(time)
 // console.log(moment().set('hour',9))
 
 // localStorage of time-block content
-let entry = JSON.parse(localStorage.getItem('events')) || []
-entry.forEach(entries => {
-  let eventElem = document.getElementsByClassName('description')
-  eventElem.textContent = document.getElementsByClassName('description').textContent
+// let entry = JSON.parse(localStorage.getItem('events')) || []
+// entry.forEach(entries => {
+//   let eventElem = document.getElementsByClassName('description')
+//   eventElem.textContent = document.getElementsByClassName('description').textContent
 
   // document.getElementsByClassName('description').append(eventElem)
-})
+// })
+
+document.getElementsByClassName('description').value = localStorage.getItem('events')
 
 document.addEventListener('click', event => {
     if(event.target.classList.contains('saveBtn')) {
-      console.log('ping')
+      
+      // let entries = document.getElementsByClassName('description')
+      // for(let i=0; i<entries.length; i++) {
+      //   let entryElem = ''
+      //   entryElem += (entries[i].value)
+      //   console.log(entryElem) 
+      //   localStorage.setItem('events', entryElem)
+      // }
 
-      let entries = document.getElementsByClassName('description').textContent
-
-      entry.push(entries)
-      localStorage.setItem('events', JSON.stringify(entry))
-
-      console.log(localStorage.getItem('entry'))
-
-      let eventElem = document.getElementsByClassName('description')
-      eventElem.textContent = document.getElementsByClassName('description').textContent
-      // document.getElementsByClassName('description').append(eventElem)
     }
 })
 
-
-// document.getElementsByClassName('saveBtn').addEventListener('click', event => {
-//   if(event.target.className === 'saveBtn') {
-//     let entries = document.getElementsByClassName('description').textContent
-
-//     entry.push(entries)
-//     localStorage.setItem('events', JSON.stringify(entry))
-
-//     console.log(localStorage.getItem('entry'))
-
-//     let eventElem = document.getElementsByClassName('description')
-//     eventElem.textContent = document.getElementsByClassName('description').textContent
-//     document.getElementsByClassName('description').append(eventElem)
-//   }
-// })
